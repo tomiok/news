7e25b455b6b327de75b85b00ee21b4ac44e0ef9d
tomas lingotti <tomaslingotti@gmail.com>
2023-01-23 18:19:32 -0300

    update gitignore

--- internal/collector/job.go
+++ internal/collector/job.go
@@ -1,9 +1,12 @@
-package collector
+package main
 
 import (
 	"context"
 	"fmt"
 	"github.com/rs/zerolog/log"
+	"news/internal/collector"
+	"strings"
+	"sync"
 )
 
 var arr = []string{"h1", "h2", "h3", "h4", "h5", "h6", "div", "span", "hr", "p", "br", "b", "i", "strong", "em", "ol", "ul", "li", "pre", "code", "blockquote", "article", "section"}
@@ -13,50 +16,58 @@ type Job interface {
 }
 
 type AggregateJob struct {
-	Scanner   Scanner
-	Collector Collector
-	Storage   Storage
+	Scanner   collector.Scanner
+	Collector collector.Collector
+	Storage   collector.Storage
 }
 
 func NewJob() *AggregateJob {
 	return &AggregateJob{
-		Collector: NewCollector(),
-		Scanner:   NewSiteScanner(),
+		Collector: collector.NewCollector(),
+		Scanner:   collector.NewSiteScanner(),
 	}
 }
 
 func (a *AggregateJob) Do() {
-	sites := a.Scanner.Scan()
-	chSites := make(chan Site)
-	chArticles := make(chan RawArticle)
+	// declare channels
+	chSites := make(chan collector.Site)
+	chArticles := make(chan collector.RawArticle)
 
-	go func() {
-		for _, site := range sites {
-			chSites <- site
-		}
-		close(chSites)
-	}()
+	go a.getSites(chSites)
+	go a.getRawArticles(chSites, chArticles)
 
-	go a.do(chSites, chArticles)
 	Print(chArticles)
 }
 
-func (a *AggregateJob) do(sitesCh chan Site, articlesCh chan RawArticle) {
-	for site := range sitesCh {
-		articles, err := a.Collector.Collect(context.Background(), site)
-		if err != nil {
-			log.Warn().Err(err).Msgf("cannot found articles for %s", site.URL)
-		}
-		for _, article := range articles {
-			articlesCh <- article
-		}
-		//close(articlesCh)
-
+func (a *AggregateJob) getSites(chSites chan collector.Site) {
+	sites := a.Scanner.Scan()
+	for _, site := range sites {
+		chSites <- site
 	}
+	close(chSites)
 }
 
-func Print(articlesCh chan RawArticle) {
+func (a *AggregateJob) getRawArticles(sitesCh chan collector.Site, articlesCh chan collector.RawArticle) {
+	var wg sync.WaitGroup
+	for site := range sitesCh {
+		wg.Add(1)
+		go func(site collector.Site) {
+			articles, err := a.Collector.Collect(context.Background(), site)
+			if err != nil {
+				log.Warn().Err(err).Msgf("cannot found articles for %s", site.URL)
+			}
+			for _, article := range articles {
+				articlesCh <- article
+			}
+			wg.Done()
+		}(site)
+		wg.Wait()
+	}
+	close(articlesCh)
+}
+
+func Print(articlesCh chan collector.RawArticle) {
 	for a := range articlesCh {
-		fmt.Println(a.String())
+		fmt.Println(strings.TrimSpace(a.Title))
 	}
 }
