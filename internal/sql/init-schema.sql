ebd8bf44e38e5a1f17ce7e0303d0b16da4fe73a9
tomas lingotti <tomaslingotti@gmail.com>
2023-01-29 18:31:01 -0300

    add sites to CSV

--- internal/sql/init-schema.sql
+++ internal/sql/init-schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE articles
 (
     id          INT          NOT NULL AUTO_INCREMENT,
-    title       VARCHAR(255) NOT NULL,
+    title       VARCHAR(255) NOT NULL UNIQUE,
     description TEXT         NOT NULL,
     content     TEXT         NOT NULL,
     link        VARCHAR(255) NOT NULL,
